993adabb60c0ea8d969dcda6a65dcffad3f14d90
Marcin <nicetocodeyou@gmail.com>
2024-02-24 20:32:08 +0100

    JS string

--- 06.JavaScript/helloWorld.js
+++ 06.JavaScript/helloWorld.js
@@ -1,1 +1,65 @@
-console.log("Hello World")
+// var a = 5;
+// var b = 10;
+// var c = "i am a";
+
+
+// a = a + 1;
+// b = b + 5;
+// c = c + " String";
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log("STOP STOP")
+
+// var myVar = 87;
+// myVar = myVar + 1; // increment by 1
+// myVar++; // faster increment by 1
+// console.log(myVar) // score is 89, because we have 2 increments
+
+// var myVar = 11;
+// myVar = myVar - 1;
+// myVar--;
+// console.log(myVar)
+
+// var remainder;
+// remainder = 11 % 5;
+// console.log(remainder)
+// console.log("STOP STOP")
+// var a = 5;
+// a += 5;
+// console.log(a)
+// console.log("STOP STOP")
+// var myStr1 = "literal 'quote' test";
+// var myStr2 = 'literal "quote" test';
+// var myStr3 = "literal \"quote\\\" test";
+// console.log(myStr1)
+// console.log(myStr2)
+// console.log(myStr3)
+// console.log("STOP STOP")
+
+// myStr = "test backspace \b\nTest new line \rttsfd";
+// console.log(myStr)
+// var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+// console.log(myStr)
+// console.log("---------")
+// var myName = "Martin";
+// var myStr = "My name is " + myName + ", and i am well!";
+// console.log(myStr)
+// console.log("-----------------")
+
+// var firstName = "Marcin"
+// var sureName = " Martin"
+// firstName += sureName
+// console.log(firstName)
+// console.log("-----------------")
+// var firstNameLength = 0;
+var firstName = "Ada";
+// firstName = firstName.length
+console.log(firstName.length)
+
+
+
+
+
+
