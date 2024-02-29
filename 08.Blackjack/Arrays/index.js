ae768f9544d8a32b6717bd8a42eac8bb39983426
Marcin <nicetocodeyou@gmail.com>
2024-02-29 21:00:49 +0100

    array

---
+++ 08.Blackjack/Arrays/index.js
@@ -0,0 +1,5 @@
+let featuredPosts = [
+    "Check out my Netflix clone",
+    "Here's he code for my project",
+    "I've just relaunched my portfolio"
+]
--- 08.Blackjack/Blackjack/index.js
+++ 08.Blackjack/Blackjack/index.js
@@ -1,11 +1,39 @@
-let firstCard = 55
-let secondCard = 5
+let firstCard = 5
+let secondCard = 3
+let cards = [firstCard, secondCard, ]
 let sum = firstCard + secondCard
+let hasBlackjack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("card-el")
+// let sumEl = document.querySelector("#sum-el")
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card?")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack!")
-} else {
-    console.log("You're out of the game!")
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wahoo! You've got Blackjack!"
+        hasBlackjack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.innerText = message
+    sumEl.innerText = "Sum: " + sum
+    cardEl.innerText = "Cards: " + cards[0] +" and "+ cards[1]
+}
+
+function newCard() {
+    console.log("Drawing new card from the deck")
+    let card = 5
+    sum += card
+    renderGame()
+    // sumEl.innerText = "Sum: " + sum
+    cardEl.innerText = "Cards: " + firstCard +" and "+ secondCard + " and " + card
 }
