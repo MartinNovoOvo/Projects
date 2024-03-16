ccb64a7b7650a9c0515b47a4fd5c2fd90e0ccdf7
Marcin <nicetocodeyou@gmail.com>
2024-03-16 11:12:51 +0100

    innerHTML

--- 10. Chrome extension/Lead trackery/index.js
+++ 10. Chrome extension/Lead trackery/index.js
@@ -1,9 +1,18 @@
-let myLeads = []
+let myLeads = [1, 2, 3]
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
  inputBtn.addEventListener("click", function() {
-   myLeads.push(document.getElementById("input-el").value);
+   myLeads.push(inputEl.value);
    console.log(myLeads);
  })
+
+for (let i = 0; i<myLeads.length; i++) {
+   ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+  // //Alternative
+  //  const li = document.createElement("li")
+  //  li.textContent = myLeads[i]
+  //  ulEl.append(li)
+}
--- 10. Chrome extension/workbook/index.js
+++ 10. Chrome extension/workbook/index.js
@@ -1,5 +1,13 @@
-let boxEl = document.getElementById("box-el")
+// let boxEl = document.getElementById("box-el")
 
-boxEl.addEventListener("click", function() {
-    console.log("I want to open the box!")
-})
+// boxEl.addEventListener("click", function() {
+//     console.log("I want to open the box!")
+// })
+
+const container = document.getElementById("container")
+
+container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+function buy() {
+    container.innerHTML += "<p>Thank you for buying!</p>"
+}
